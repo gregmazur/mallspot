a297533008f8ec9eba0fce6715cd86dc25840909
Dmitriy <cheess@ex.ua>
2015-03-19 15:25:31 +0200

    ADMIN

---
+++ mallSpot/src/org/hillel/it/mallspot/model/entity/superAdmin/superAdmin.java
@@ -0,0 +1,10 @@
+package org.hillel.it.mallspot.model.entity.superAdmin;
+
+public class superAdmin {
+
+	public static void main(String[] args) {
+		System.out.println("I'm SUPERADMIN");
+
+	}
+
+}
